dafbc60b741e10c3e28af0768b8390d2062eac32
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-02-28 00:43:03 -0500

    initial commit

---
+++ components/header/DarkHeader.jsx
@@ -0,0 +1,40 @@
+'use client';
+import Container from 'react-bootstrap/Container';
+
+
+export default function DarkHeader() {
+    return (
+        <div  className="bg-navbar-dark row">
+            <div className="links col-5  d-flex justify-content-center align-items-center pt-3">
+
+                <p className='header-nav-link mx-2' href="/home">Events</p>
+                <p className='header-nav-link mx-2' href="/home">Players</p>
+                <p className='header-nav-link mx-2' href="/home">Coaches</p>
+                 <p className='header-nav-link mx-2' href="/home">About</p>
+                 <p className='header-nav-link mx-2' href="/home">Events</p>
+                 <p className='header-nav-link mx-2' href="/home">Contact</p>
+
+
+
+            </div>
+
+            <div className="img col-2 e d-flex justify-content-center align-items-center">
+
+                <img className='img-header' src="/assets/logos/opsm_logo_black.png" alt="" />
+
+            </div>
+
+            <div className="login col-5 e">
+
+                <div className="d-flex justify-content-end align-items-center pt-3 px-4">
+                    <p className='header-nav-link mx-2 ' href="/home">Login</p>
+                    <p className='header-nav-link mx-2 red_button' href="/home">Sign Up</p>
+                        </div>
+
+            </div>
+
+
+   
+        </div>
+    );
+    }
---
+++ components/home/Hero.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Hero() {
+    return (
+
+    
+        <div className="hero_div_1 p-5">
+
+            <div className="container d-flex flex-column row mx-lg-4">
+
+                <div className="hero_div_main mt-lg-5 col-lg-10">
+
+            <div className="hero_logo_div d-flex">
+                <p className="hero_text_network mt-4 h5">OPSM NETWORK</p>
+            </div>
+
+            <div className="hero_title_div">
+                <p className="display-3 " id="hero_title"> <b>The Trusted North American Soccer Network </b></p>
+            </div>
+
+            <div className="hero_text_div">
+                <p className="">
+                OPSM Network empowers players and coaches with objective metric-based recruitment decisions. With just a click, share your profile with our trusted network of thousands of coaches and scouts and receive that career push you’ve always dreamed of.       
+</p>
+            </div>
+
+            <div className="hero_button_div my-4">
+
+                <button className="btn  hero_button">JOIN THE NETWORK</button>
+
+            </div>
+            
+
+            
+
+
+
+        </div>
+        </div>
+        </div>
+    );
+    }
