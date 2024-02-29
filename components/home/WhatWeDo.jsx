332b30cf290798e6c5620e7200521a078e4127de
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-02-28 23:29:38 -0500

    carousel added

--- components/home/WhatWeDo.jsx
+++ components/home/WhatWeDo.jsx
@@ -1,58 +1,51 @@
 'use client'
-import React from "react"
+import React, { useEffect } from "react"
+import { useState } from "react"
 import { useKeenSlider } from "keen-slider/react"
 import "keen-slider/keen-slider.min.css"
 import './carousel.css'
 
 export default () => {
   const [sliderRef] = useKeenSlider({
-    loop: true,
+    loop: false,
     mode: "free-snap",
     slides: {
-      perView: 1.6,
-      spacing: 0,
+      perView: 1.7,
+      spacing: 10,
     },
   })
 
+  
+
+
+ 
+
   return (
-
-
-
-    <div className="ml-5 my-5 pt-2">
-
+   
+    <div className="ml-lg-5 my-lg-5 pt-2">
         <div className="container my-5">
-
            <div className="row">
-            <div className="col-6">
-                <h1 className="title-text ">What Makes Us The Best At What We Do?</h1>
+            <div className="col-lg-6 col-12 ">
+                <h1 className="title-text text-center text-lg-start ">What Makes Us The Best At What We Do?</h1>
             </div>
            </div>
-
-
-        </div>
-
-        
-
-
-
+      </div>
     <div ref={sliderRef} className="keen-slider">
-      <div className="keen-slider__slide number-slide1">
-        <img src="/assets/images/home/carousel_image_1.png" alt="" />
+      <div className="keen-slider__slide number-slide1 mx-lg-2">
+        <img src="/assets/images/home/carousel_image_1.png"  width={'100%'} alt="" />
       </div>
-      <div className="keen-slider__slide number-slide2">
-                <img src="/assets/images/home/carousel_image_2.png" alt="" />
+      <div className="keen-slider__slide number-slide2 mx-lg-2">
+                <img src="/assets/images/home/carousel_image_2.png" width={'100%'} alt="" />
       </div>
-      <div className="keen-slider__slide number-slide3">
-                <img src="/assets/images/home/carousel_image_3.png" alt="" />
+      <div className="keen-slider__slide number-slide3 mx-lg-2">
+                <img src="/assets/images/home/carousel_image_3.png" width={'100%'} alt="" />
 
       </div>
-      <div className="keen-slider__slide number-slide4">
-                <img src="/assets/images/home/carousel_image_4.png" alt="" />
+      <div className="keen-slider__slide number-slide4 mx-lg-2">
+                <img src="/assets/images/home/carousel_image_4.png" width={'100%'} alt="" />
 
       </div>
       </div>
-   
-    </div>
-
+  </div>
   )
 }
