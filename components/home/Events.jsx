b6ac6a42ab245d27a365869b09b95649c65900b7
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-03-01 13:39:32 -0500

    added events to home

---
+++ components/home/Events.jsx
@@ -0,0 +1,66 @@
+export default function Events() {
+    return (
+
+<div className="content my-lg-5 py-lg-5">
+ 
+            <div className="title d-flex row my-4">
+                <div className="col-lg-6 col-12 my-3 text-center text-lg-start">
+                    <h1 className="ms-lg-5 ps-lg-5 title-text">PREMIER EVENTS</h1>
+                    <p className="ms-lg-5 ps-lg-5">Providing access to personalized recruitment services, player evaluation, and career advancement opportunities. Apply now for an upcoming event. </p>
+                </div>
+
+
+
+            </div>
+
+            <div className="row my-5 mx-5 px-2 flex justify-content-between">
+
+                <div className="col-lg-3 col-12 d-flex flex-column">
+
+                    <img src="/assets/images/home/professional_combines.png" alt="" />
+                    <button className="btn btn-primary text-white my-3">APPLY NOW</button>
+
+
+
+
+                </div>
+
+                <div className="col-lg-3 col-12 d-flex flex-column">
+
+                    <img src="/assets/images/home/collegiate_combines.png" alt="" />
+                    <button className="btn btn-primary text-white my-3">APPLY NOW</button>
+
+
+
+
+                </div>
+
+               <div className="col-lg-3 col-12 d-flex flex-column">
+
+                    <img src="/assets/images/home/id_camps.png" alt="" />
+                    <button className="btn btn-primary text-white my-3">APPLY NOW</button>
+
+
+
+
+                </div>
+
+                 <div className="col-lg-3 col-12 d-flex flex-column">
+
+                    <img src="/assets/images/home/residency_camps.png" alt="" />
+                    <button className="btn btn-primary text-white my-3">APPLY NOW</button>
+
+
+
+
+                </div>
+
+
+            </div>
+
+            </div>
+
+
+        
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import WhatWeDo from "../../components/home/WhatWeDo";
 import Footer from "../../components/footer/Footer";
 import Empowering from "../../components/home/Empowering";
 import NextLevel from "../../components/home/NextLevel";
+import Events from "../../components/home/Events";
 export default function Home() {
   return (
     <div>
@@ -12,6 +13,7 @@ export default function Home() {
       <WhatWeDo className="my-4" />
       <Empowering />
       <NextLevel />
+      <Events />
       <Footer />
       
     </div>
