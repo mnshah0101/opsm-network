dad513ea99dacb614cadda536e2854d029f3e14b
mnshah0101 <110360393+mnshah0101@users.noreply.github.com>
2024-03-02 21:16:28 -0500

    fixed spacing on home page

--- components/home/Partnerships.jsx
+++ components/home/Partnerships.jsx
@@ -1,6 +1,6 @@
 export default function Partnerships() {
     return (
-        <div className="my-5 py-5 partnerships-div">
+        <div className="my-5 py-5 mx-0 px-5 partnerships-div">
             <img src="/assets/images/home/partnerships.png" alt="" />
 
         </div>
--- components/home/Revolutionalizing.jsx
+++ components/home/Revolutionalizing.jsx
@@ -1,7 +1,7 @@
 export default function Revolutionalizing() {
 
     return (
-        <div className="revolution-div row p-5  ">
+        <div className="revolution-div row p-5  mx-0 ">
 
             <div className="text-div mx-lg-5 mt-5 pt-5 ">
     <h1 className="col-lg-8 col-12 text-center text-lg-start font-rem-5-bold text-uppercase text-white">Revolutionizing Soccer Recruitment</h1>
